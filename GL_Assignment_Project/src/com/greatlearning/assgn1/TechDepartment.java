efe4dc5389310bf3cb4c020eaa0afa6ce59a63b4
gsgit2021 <gaurav.sys@gmail.com>
2021-11-28 17:44:13 +0400

    Code Submit

---
+++ GL_Assignment_Project/src/com/greatlearning/assgn1/TechDepartment.java
@@ -0,0 +1,17 @@
+package com.greatlearning.assgn1;
+
+public class TechDepartment extends SuperDepartment {
+	public void departmentName() {
+		System.out.println("Welcome to Tech Department ");
+	}
+
+	public void getTodaysWork() {
+		System.out.println("Complete coding of module 1");
+	}
+	public void getWorkDeadline() {
+		System.out.println("Complete by EOD");
+	}
+	public void getTechStackInformation() {
+		System.out.println("core Java");
+	}
+}
